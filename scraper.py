e11bcd272c7d70a94b4e4cce3ad4aa5de7bcf7b8
qqphot <qqphot@users.noreply.github.com>
2015-10-26 16:53:41 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,8 +6,19 @@ from multiprocessing.dummy import Pool
 from multiprocessing import cpu_count
 import grequests
 
-start_urls = ['http://www.amazon.com/best-sellers-books-Amazon/zgbs/books/ref=zg_bs_unv_b_1_173508_2',
-            'http://www.amazon.com/Best-Sellers-Toys-Games/zgbs/toys-and-games/ref=zg_bs_unv_t_1_2514571011_2']
+start_urls = ['http://www.amazon.com/Best-Sellers-Books-Arts-Photography/zgbs/books/1/ref=zg_bs_nav_b_1_b', 
+'http://www.amazon.com/Best-Sellers-Books-Business-Money/zgbs/books/3/ref=zg_bs_nav_b_1_b', 
+'http://www.amazon.com/Best-Sellers-Books-Computers-Technology/zgbs/books/5/ref=zg_bs_nav_b_1_b', 
+'http://www.amazon.com/Best-Sellers-Books-Education-Teaching/zgbs/books/8975347011/ref=zg_bs_nav_b_1_b', 
+'http://www.amazon.com/Best-Sellers-Books-Engineering-Transportation/zgbs/books/173507/ref=zg_bs_nav_b_1_b', 
+'http://www.amazon.com/Best-Sellers-Books-History/zgbs/books/9/ref=zg_bs_nav_b_1_b', 
+'http://www.amazon.com/Best-Sellers-Books-Law/zgbs/books/10777/ref=zg_bs_nav_b_1_b', 
+'http://www.amazon.com/Best-Sellers-Books-Medical/zgbs/books/173514/ref=zg_bs_nav_b_1_b', 
+'http://www.amazon.com/Best-Sellers-Books-Politics-Social-Sciences/zgbs/books/3377866011/ref=zg_bs_nav_b_1_b', 
+'http://www.amazon.com/Best-Sellers-Books-Reference/zgbs/books/21/ref=zg_bs_nav_b_1_b', 
+'http://www.amazon.com/Best-Sellers-Books-Science-Math/zgbs/books/75/ref=zg_bs_nav_b_1_b', 
+'http://www.amazon.com/Best-Sellers-Books-Test-Preparation/zgbs/books/5267710011/ref=zg_bs_nav_b_1_b', 
+'http://www.amazon.com/Best-Sellers-Books-Textbooks/zgbs/books/465600/ref=zg_bs_nav_b_1_b']
 pool = Pool(cpu_count() * 20)
 
 def scrape(response, **kwargs):
